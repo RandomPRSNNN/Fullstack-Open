c5706770179214827fdec600cb0a1b3694fed216
Edwin Valentelis <edwin.pow@gmail.com>
2026-03-02 13:35:33 +0000

    1.2

--- part1/components/Content.jsx
+++ part1/components/Content.jsx
@@ -1,6 +1,12 @@
-const Content = (props) =>{
-    return(
-        <p>{props.title}: {props.exercises}</p>
+import Part from "./Part"
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part title={props.data[0].title} exercises={props.data[0].exercises} />
+            <Part title={props.data[1].title} exercises={props.data[1].exercises} />
+            <Part title={props.data[2].title} exercises={props.data[2].exercises} />
+        </div>
     )
 }
 
--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,19 +4,19 @@ import Total from "../components/Total"
 
 const App = () => {
   const course = 'Half Stack application development'
+
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const partArr = [{title: part1, exercises: exercises1}, {title: part2, exercises: exercises2}, {title: part3, exercises: exercises3}];
 
   return (
     <>
       <Header course={course}/>
-      <Content title={part1} exercises={exercises1}/>
-      <Content title={part2} exercises={exercises2}/>
-      <Content title={part3} exercises={exercises3}/>
+      <Content data={partArr}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </>
   )
