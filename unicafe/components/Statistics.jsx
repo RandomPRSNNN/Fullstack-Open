8da44762e84500112c30adda7e1cb6936c7e9dee
Edwin Valentelis <edwin.pow@gmail.com>
2026-03-02 18:24:41 +0000

    1.10

--- unicafe/components/Statistics.jsx
+++ unicafe/components/Statistics.jsx
@@ -1,16 +1,18 @@
+import StatisticLine from "./StatisticLine";
+
 const Statistics = ({good, bad, neutral}) => {
     let total = good + bad + neutral;
     let average = total === 0 ? 0 : (good - bad) / total;
     let positivePercentage = total === 0 ? 0 : (good / total) * 100;
 
     if (total === 0) return (<p>No feedback given yet</p>);
-    
+
     return (
         <div>
             <h2>Statistics</h2>
-            <p>Good {good}</p>
-            <p>Neutral {neutral}</p>
-            <p>Bad {bad}</p>
+            <StatisticLine text="Good" value={good} />
+            <StatisticLine text="Neutral" value={neutral} />
+            <StatisticLine text="Bad" value={bad} />
             <br/>
             <p>Total {total}</p>
             <p>Average {average}</p>
