01238966df1fc0765dd6b8097f59cad5bc7f4cf3
Edwin Valentelis <edwin.pow@gmail.com>
2026-03-03 14:16:26 +0000

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filterList, setFilterList] = useState([])
 
   const addNewPerson = (event) => {
     event.preventDefault()
@@ -14,13 +19,12 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    else if(persons.some(person => person.phone === newPhone))
-    {
+    else if (persons.some(person => person.number === newPhone)) {
       alert(`${newPhone} is already added to phonebook`)
       return
     }
 
-    const newInputPerson = { name: newName, phone: newPhone }
+    const newInputPerson = { name: newName, number: newPhone, id: persons.length + 1 }
 
     setPersons(persons.concat(newInputPerson))
     setNewName('')
@@ -35,10 +39,18 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    let filteredPersons = persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    setFilterList(filteredPersons)
+  }
+
+  const listToShow = filterList.length > 0 ? filterList : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
-
+      <p>Filter to show with <input onChange={handleFilterChange} /></p>
+      <br />
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName} onChange={handleNameInput} />
@@ -53,7 +65,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.phone}</li>)}
+        {listToShow.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
