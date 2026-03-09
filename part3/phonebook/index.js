5f3f52211e27381d8fc593aff60b1503492677a7
Edwin Valentelis <edwin.pow@gmail.com>
2026-03-09 10:30:28 +0000

    3.9 to 3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 const morgan = require('morgan')
 app.use(express.json())
+app.use(express.static('dist'))
 
 let morganConfig = morgan(function (tokens, request, response) {
     if (tokens.method(request) == 'POST') {
@@ -115,7 +118,7 @@ app.get('/info', (request, response) => {
     response.status(200)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/phonebook/readMe.md
@@ -0,0 +1,2 @@
+URL to deployed backend server:
+https://fullstack-open-part3-backend-za69.onrender.com/api/persons
