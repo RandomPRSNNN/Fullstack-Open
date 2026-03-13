9c80bcfbf99a2f916adfb064915eae4737c3cb40
Edwin Valentelis <edwin.pow@gmail.com>
2026-03-13 14:58:20 +0000

    test fix

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,46 +1,7 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
-
-const listOfBlogs = [
-    {
-        title: 'Edwin in the world',
-        author: 'James May',
-        url: 'nyt.com',
-        likes: 3
-    },
-    {
-        title: 'Eating Chicken with cheese',
-        author: 'James May',
-        url: 'google.com',
-        likes: 2
-    },
-    {
-        title: 'How to dance like the young kids',
-        author: 'James May',
-        url: 'yahoo.com',
-        likes: 7
-    },
-    {
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-        likes: 5
-    },
-    {
-        title: 'How to talk to cheese',
-        author: 'Maire Joe',
-        url: 'lovebooks.com',
-        likes: 11
-    }
-]
-
-const singleBlog = {
-    title: 'How to add a new blog',
-    author: 'Betsy Gold',
-    url: 'https://homepages.cwi.nl/~storm/',
-    likes: 7
-}
+const testHelper = require('./test_helper')
 
 describe('List helper tests', () => {
     test('dummy returns one', () => {
@@ -50,27 +11,25 @@ describe('List helper tests', () => {
         assert.strictEqual(result, 1)
     })
 
-
     test('when list has only one blog, equals the likes of that', () => {
-        const result = listHelper.totalLikes([singleBlog])
+        const result = listHelper.totalLikes([testHelper.singleBlog])
 
         assert.strictEqual(result, 7)
     })
 
-
     test('blog with the most likes', () => {
-        const result = listHelper.favoriteBlog(listOfBlogs)
-        assert.deepStrictEqual(result, listOfBlogs[4]);
+        const result = listHelper.favoriteBlog(testHelper.listOfBlogs)
+        assert.deepStrictEqual(result, testHelper.listOfBlogs[4]);
     })
 
 
     test('author with the most number of blogs', () => {
-        const result = listHelper.mostBlogs(listOfBlogs)
+        const result = listHelper.mostBlogs(testHelper.listOfBlogs)
         assert.deepStrictEqual(result, { author: 'James May', blogs: 3 });
     })
 
     test('the author who has the most number of likes', () => {
-        const result = listHelper.mostLikes(listOfBlogs)
+        const result = listHelper.mostLikes(testHelper.listOfBlogs)
         assert.deepStrictEqual(result, { author: 'James May', likes: 12 });
     })
 }) 
